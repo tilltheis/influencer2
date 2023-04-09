5ddff62a32574099f626018bd7eca84a57710372
Till Theis <theis.till@gmail.com>
2023-04-09 13:38:41 +0200

    remove "api" route prefix because that's an unrelated responsibility

--- src/main/scala/influencer2/http/AppRouter.scala
+++ src/main/scala/influencer2/http/AppRouter.scala
@@ -9,27 +9,27 @@ import zio.{UIO, URLayer, ZIO, ZLayer}
 
 class AppRouter(jwtCodec: JwtCodec, appController: AppController):
   private def privateRoutes(sessionUser: SessionUser): UHttpApp = Http.collectZIO[Request] {
-    case request @ Method.DELETE -> !! / "api" / "users" / username => dummyResponse(request)
+    case request @ Method.DELETE -> !! / "users" / username => dummyResponse(request)
 
-    case request @ Method.DELETE -> !! / "api" / "sessions" => appController.handleDeleteSession(request)
+    case request @ Method.DELETE -> !! / "sessions" => appController.handleDeleteSession(request)
 
-    case request @ Method.GET -> !! / "api" / "feeds" / username => dummyResponse(request)
+    case request @ Method.GET -> !! / "feeds" / username => dummyResponse(request)
 
-    case request @ Method.PUT -> !! / "api" / "posts" / postId                         => dummyResponse(request)
-    case request @ Method.PUT -> !! / "api" / "posts" / postId / "likes" / username    => dummyResponse(request)
-    case request @ Method.DELETE -> !! / "api" / "posts" / postId / "likes" / username => dummyResponse(request)
+    case request @ Method.PUT -> !! / "posts" / postId                         => dummyResponse(request)
+    case request @ Method.PUT -> !! / "posts" / postId / "likes" / username    => dummyResponse(request)
+    case request @ Method.DELETE -> !! / "posts" / postId / "likes" / username => dummyResponse(request)
 
-    case request @ Method.GET -> !! / "api" / "notifications" / username => dummyResponse(request)
+    case request @ Method.GET -> !! / "notifications" / username => dummyResponse(request)
   }
 
   private val publicRoutes: UHttpApp = Http.collectZIO[Request] {
-    case request @ Method.PUT -> !! / "api" / "users" / username => appController.handleCreateUser(request)
+    case request @ Method.PUT -> !! / "users" / username => appController.handleCreateUser(request)
 
-    case request @ Method.POST -> !! / "api" / "sessions" => appController.handleCreateSession(request)
+    case request @ Method.POST -> !! / "sessions" => appController.handleCreateSession(request)
 
-    case request @ Method.GET -> !! / "api" / "posts" if request.url.queryParams.get("username").isDefined =>
+    case request @ Method.GET -> !! / "posts" if request.url.queryParams.get("username").isDefined =>
       dummyResponse(request)
-    case request @ Method.GET -> !! / "api" / "posts" / postId => dummyResponse(request)
+    case request @ Method.GET -> !! / "posts" / postId => dummyResponse(request)
   }
 
   private def authenticate(authenticatedRoutes: SessionUser => UHttpApp): UHttpApp =
